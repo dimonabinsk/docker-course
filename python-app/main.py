f40be88145ce44db7780c86801484a5fcc948929
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-03-29 19:02:22 +0300

    update

--- python-app/main.py
+++ python-app/main.py
@@ -6,4 +6,4 @@ year = int(input("Пожалуйста введите год: "))
 month = int(input("Введите номер любого месяца: "))
 
 print(calendar.month(year, month))
-print("Всего хорошего")
+print("Всего хорошего!")
